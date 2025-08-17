5291482cc47ff0ef0a8f57a1d65a49c09fe974c8
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-17 20:20:53 +0530

    Update provider.tf
    removed credentials

--- azure/provider.tf
+++ azure/provider.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
     features {}
 
-    subscription_id =  "9dcd2a18-85b5-41cb-b256-9cf315c49e2f"
-    client_id = "0d3e430d-eb96-4fc7-9b70-76591bf4ebe5"
-    client_secret = "dCz8Q~WhRThSEGIMn~4kTo9PfMwrSr1xgcyvFc4m"
-    tenant_id = "47ee8335-fbc5-419e-a6d8-c9cd24e73a7e"
+    subscription_id =  "--"
+    client_id = "--"
+    client_secret = "--"
+    tenant_id = "--"
 
 }
+
